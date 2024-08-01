6918c191c99b23e005d0cf0d22061c32bd36da5d
Dorian1218 <dmitchell1663@gmail.com>
2024-08-01 16:41:21 -0400

    decorated navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,8 +6,8 @@ import { ModeToggle } from './togglebutton'
 
 const Navbar = () => {
   return (
-    <div className='w-screen flex items-center justify-between p-5'>
-        <h1>Connect</h1>
+    <div className='w-screen flex items-center justify-between p-5 fixed top-0'>
+        <h1 className='text-2xl underline decoration-blue-500 -rotate-2'>Connect</h1>
         <div className='flex gap-5 items-center'>
             <Link href="/forum">Forum</Link>
             <Link href="/calendar">Calendar</Link>
