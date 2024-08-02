022e892382fb3d03fa86ffc24b9fc55be63b139e
lastusername <justindm1914@gmail.com>
2024-08-02 00:42:38 -0400

    added selection date

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,6 +3,8 @@
 import Link from 'next/link'
 import React from 'react'
 import { ModeToggle } from './togglebutton'
+import { cn } from "@/lib/utils"
+
 import {
   Dialog,
   DialogContent,
@@ -14,7 +16,24 @@ import {
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Textarea } from "@/components/ui/textarea"
+import {Popover, PopoverContent, PopoverTrigger,} from "@/components/ui/popover"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,} from "@/components/ui/form"
 
+/*export function CalendarForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  })
+ 
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  } */
 
 const Navbar = () => {
   return (
@@ -32,14 +51,60 @@ const Navbar = () => {
                 <div className='flex flex-col gap-y-4'>
                   <Input placeholder="Title"/>
                   <Textarea  className = "max-h-60" placeholder="Description"/>
-                  <Input />
+                
+                  <Form>
+      <form className="space-y-8">
+        <FormField
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Date of birth</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) =>
+                      date > new Date() || date < new Date("1900-01-01")
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                Your date of birth is used to calculate your age.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
                   <Input />
                 </div>
               </DialogDescription>
             </DialogHeader>
           </DialogContent>
         </Dialog>
-
         <ModeToggle />
       </div>
     </div>
