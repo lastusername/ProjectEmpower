562b1ada69d49fb2406333efbb6e9b131a6efd1f
Dorian1218 <dmitchell1663@gmail.com>
2024-08-01 22:26:19 -0400

    made calendar larger, added create event modal

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,16 +3,43 @@
 import Link from 'next/link'
 import React from 'react'
 import { ModeToggle } from './togglebutton'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 const Navbar = () => {
   return (
     <div className='w-screen flex items-center justify-between p-5 fixed top-0'>
-        <h1 className='text-2xl underline decoration-blue-500 -rotate-2'>Connect</h1>
-        <div className='flex gap-5 items-center'>
-            <Link href="/forum">Forum</Link>
-            <Link href="/calendar">Calendar</Link>
-            <ModeToggle />
-        </div>
+      <h1 className='text-2xl underline decoration-blue-500 -rotate-2'>Connect</h1>
+      <div className='flex gap-5 items-center'>
+        <Link href="/forum">Forum</Link>
+        <Link href="/calendar">Calendar</Link>
+        <Dialog>
+          <DialogTrigger>Create Event</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create Event</DialogTitle>
+              <DialogDescription>
+                <div className='flex flex-col gap-y-4'>
+                  <Input />
+                  <Input />
+                  <Input />
+                  <Input />
+                </div>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+
+        <ModeToggle />
+      </div>
     </div>
   )
 }
